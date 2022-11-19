66095683357dd11a81659d45790cf7bfa3ccb878
Đàm Đình hưng <117186163+Hugnh26n10ne@users.noreply.github.com>
2022-11-19 21:37:50 +0700

    Đây là file gốc mọi người clone về nhé

---
+++ quanlihocvien.c
@@ -0,0 +1,25 @@
+//Du an -> Quan li cac  ban hoc vien
+#include<stdio.h>
+
+struct hoc_vien{
+	int stt;
+	char mahv;
+	char hoten;
+	char gioitinh;
+	int ngaysinh;
+	char quequan;
+	char lop;
+	char tenmon;
+	char mamon;
+	char sobuoi;
+	char sobuoidi;
+	int diemdk;
+	int diemthi;
+	int dtb;
+	char xeploai="-";
+};
+
+main()
+{
+	
+}
